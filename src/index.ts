0e285e7deda305a93686afcbc5c3c1f65d750988
S1LV3R <s1lv3r@corax.team>
2021-01-18 22:38:27 +0100

    feat: idek so much stuff

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { config } from "dotenv";
+config();
+
+import Logger from "./util/Logger";
+
+const info = "This uses info level";
+const verbose = "And this has verbose";
+const debug = "And i dont even know why you are debugging at this point";
+
+export function doSomeStuff(
+  infoMuch: string,
+  verboseMore: string,
+  debugWhy: string[]
+): void {
+  Logger.info(infoMuch);
+  Logger.verbose(verboseMore);
+  Logger.debug(debugWhy);
+}
+
+doSomeStuff(info, verbose, [debug]);
+// TODO: more examples
---
+++ src/util/Logger.ts
@@ -0,0 +1,32 @@
+import winston from "winston";
+
+const transports = [];
+
+transports.push(
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp({ format: "HH:mm:ss.SSS" }),
+      winston.format.printf(({ level, message, timestamp }) => {
+        return `[${timestamp}] ${level}: ${message}`;
+      })
+    ),
+  })
+);
+
+const Logger = winston.createLogger({
+  level: process.env.LOGGER_LEVEL ?? "debug",
+
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    silly: 4,
+    debug: 5,
+  },
+
+  transports,
+});
+
+export default Logger;
